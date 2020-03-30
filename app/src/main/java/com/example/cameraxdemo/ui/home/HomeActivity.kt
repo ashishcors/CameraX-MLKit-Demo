70747b854c24013a10ea21b989b14f6d6df82b98
Ashish Suman <ashishsumancse@gmail.com>
2020-03-30 11:35:56 +0530

    added pinch to zoom (not working, might me device dependent)

--- app/src/main/java/com/example/cameraxdemo/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/cameraxdemo/ui/home/HomeActivity.kt
@@ -7,6 +7,7 @@ import android.util.DisplayMetrics
 import android.util.Log
 import android.util.Size
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.camera.core.Camera
@@ -195,6 +196,7 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>() {
 
   @SuppressLint("ClickableViewAccessibility")
   private fun setUpTapToFocus() {
+    //Todo: Fix the onTouchListener, we only need tap event
     binding.previewView.setOnTouchListener { _, event ->
       if (event.action != MotionEvent.ACTION_UP) {
         return@setOnTouchListener true
@@ -220,6 +222,31 @@ class HomeActivity : BaseActivity<ActivityHomeBinding, HomeViewModel>() {
       }, cameraExecutor)
 
       return@setOnTouchListener true
+    }
+  }
+
+  @SuppressLint("ClickableViewAccessibility")
+  private fun setUpPinchToZoom() {
+    //TODO fix this
+    val listener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+      override fun onScale(detector: ScaleGestureDetector): Boolean {
+        val cameraControl = camera?.cameraControl
+        val currentZoomRatio: Float = camera?.cameraInfo?.zoomState?.value?.linearZoom ?: 1F
+        val delta = detector.scaleFactor
+        cameraControl?.setZoomRatio(currentZoomRatio * delta)
+        val future = cameraControl?.setZoomRatio(currentZoomRatio * delta)
+        future?.addListener(Runnable {
+          //can get focusing result using future.get()
+
+        }, cameraExecutor)
+        return true
+      }
+    }
+
+    val scaleGestureDetector = ScaleGestureDetector(this, listener)
+
+    binding.overlayContainer.setOnTouchListener { _, event ->
+      return@setOnTouchListener scaleGestureDetector.onTouchEvent(event)
     }
   }
 
